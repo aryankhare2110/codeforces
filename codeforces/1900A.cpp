faf8e2d5570d0d8b28d485f1dccd86b4efc2ca59
aryankhare2110 <aryan24124@iiitd.ac.in>
2025-07-04 13:59:28 +0530

    More Codes

---
+++ codeforces/1900A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        string s;
+        cin >> n;
+        cin >> s;
+        int c = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ".") {
+                if (s[i + 1] == "." && s[i+2] == ".") {
+                    c = 2;
+                    break; 
+                }
+                else {
+                    c++;
+                }
+            }
+        }
+        cout << c << endl;
+    }
+}
